b3b42fdea3d9f67c5df72170a35a2e4d2b1b85ea
MarcinTester <marcintester19@gmail.com>
2022-04-14 22:49:58 +0200

    text format

--- cypress/integration/practice/practiceTest.js
+++ cypress/integration/practice/practiceTest.js
@@ -1,28 +1,22 @@
 /// <reference types="Cypress" />
 
-describe('practice', function() 
-{
+describe('practice', function () {
 
-it('My FirstTest case',function() {
+  it('My FirstTest case', function () {
 
 
-cy.visit("https://www.booking.com/")
+    cy.visit("https://www.booking.com/")
 
-cy.get('.bui-calendar').then(element => {
-             
-    if (element.not(':visible')){
+    cy.get('.bui-calendar').then(element => {
+
+      if (element.not(':visible')) {
         cy.log(element.not(':visible'))
         cy.get("div[class='xp__dates xp__group']").click()
-    }
+      }
+    })
+
   })
-
-
-
-}  )
-
-
-
-}  )
+})
 
 
 
